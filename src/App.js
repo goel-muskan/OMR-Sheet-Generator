fea80f7101c1daaf62893cdf3b0e89bdaf99a672
goel-muskan <goyal3366@gmail.com>
2024-06-08 02:48:47 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import OMRSheet from './components/OmrSheet';
+
+function App() {
+  return (
+    <div className="App">
+      <OMRSheet />
+    </div>
+  );
+}
+
+export default App;
